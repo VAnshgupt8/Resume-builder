0022421fbb1263d218ea232753847c6771cd5885
buddy__44 <156524605+VAnshgupt8@users.noreply.github.com>
2026-05-18 14:50:03 +0530

    pass

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,19 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
 
 mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    const PORT = process.env.PORT || 10000;
+    const PORT = process.env.PORT || 5000;
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
