23cbe066d2b77bd503e3522a04a60fa4532e1661
buddy__44 <156524605+VAnshgupt8@users.noreply.github.com>
2026-05-18 13:37:37 +0530

    fixed vercel backend crash

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,29 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+
+//     await mongoose.connect(process.env.MONGO_URI);
+
+//     console.log('MongoDB Connected');
+
+//   } catch (error) {
+
+//     console.log(error);
+
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI Missing');
+    }
 
     await mongoose.connect(process.env.MONGO_URI);
 
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,26 @@
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const cors = require('cors');
+
+// const connectDB = require('./config/db');
+
+// dotenv.config();
+
+// connectDB();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Backend Running Successfully');
+// });
+
+// app.use('/api/auth', require('./routes/authRoutes'));
+// app.use('/api/resume', require('./routes/resumeRoutes'));
+
+// module.exports = app;
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -6,9 +29,9 @@ const connectDB = require('./config/db');
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
