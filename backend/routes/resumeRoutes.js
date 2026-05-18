5f079cde8e06ddaf719bf3748183f11918fe3b1a
buddy__44 <156524605+VAnshgupt8@users.noreply.github.com>
2026-05-18 14:28:15 +0530

    2

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const protect = require('../middleware/authMiddleware');
+const protect = require('../Middleware/authMiddleware');
 
 const {
   createResume,
