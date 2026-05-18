8ebcc98f76ac2bbf334247ad2c7c4549e31a4813
buddy__44 <156524605+VAnshgupt8@users.noreply.github.com>
2026-05-18 13:23:18 +0530

    fixed duplicate mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,87 +1,23 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
+const connectDB = require('./config/db');
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch((err) => console.log(err));
-
 app.get('/', (req, res) => {
-  res.send('Resume Builder API Running');
+  res.send('Backend Running Successfully');
 });
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/resume', require('./routes/resumeRoutes'));
 
 module.exports = app;
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-// const connectDB = require('./config/db');
-
-// const authRoutes = require('./routes/authRoutes');
-// const resumeRoutes = require('./routes/resumeRoutes');
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/resume', resumeRoutes);
-
-// app.get('/', (req, res) => {
-//   res.send('Resume Builder API Running');
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// // app.listen(PORT, () => {
-// //   console.log(`Server running on port ${PORT}`);
-// // });
-// if (process.env.NODE_ENV !== 'production') {
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
-// }
-
-// module.exports = app;
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-
-// const connectDB = require('./config/db');
-
-// const authRoutes = require('./routes/authRoutes');
-// const resumeRoutes = require('./routes/resumeRoutes');
-
-// dotenv.config();
-
-// connectDB();
-
-// const app = express();
-
-// app.use(cors());
-
-// app.use(express.json());
-
-// app.get('/', (req, res) => {
-//   res.send('Resume Builder API Running');
-// });
-
-// app.use('/api/auth', authRoutes);
-
-// app.use('/api/resume', resumeRoutes);
-
-// module.exports = app;
