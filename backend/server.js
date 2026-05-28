80502581c8e7f009d546a61847c1aa4f8d3b0829
buddy__44 <156524605+VAnshgupt8@users.noreply.github.com>
2026-05-28 22:41:38 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+const resumeRoutes = require("./routes/resumeRoutes");
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://resume-builder-6-vx4y.onrender.com/' 
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-
     const PORT = process.env.PORT || 5000;
-
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
