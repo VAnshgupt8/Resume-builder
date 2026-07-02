3df8b29a091369810ebdb682b5eb1af612711477
buddy__44 <156524605+VAnshgupt8@users.noreply.github.com>
2026-07-02 13:37:07 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://resume-builder-frontend.vercel.app",
+  credentials: true
+}));
 
 app.use(express.json());
 
